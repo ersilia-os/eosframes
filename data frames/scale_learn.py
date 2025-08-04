f3c6784515e54c37dc997b0aa85fc5c1cab7a907
ziminqian <ziminq@stanford.edu>
2025-08-04 12:47:02 -0700

    inference change

--- data frames/scale_learn.py
+++ data frames/scale_learn.py
@@ -4,28 +4,16 @@ class Scale:
     def __init__(self, robust_scalar, power_transformer):
         self.robust_scalar = robust_scalar
         self.power_transformer = power_transformer
+        self.transform = False
 
     def fit(self, df):
         df_scaled = scalarize(df, self.power_transformer, self.robust_scalar) # Using RobustScaler
+        self.transform = True
         return df_scaled
         #return transformer?
 
-#from scalarizer import scalarize
-
-# class Scale:
-#     def __init__(self, robust_scalar, power_transformer):
-#         self.robust_scalar = robust_scalar
-#         self.power_transformer = power_transformer
-#         self.scaler_ = None  # Will store fitted transformer
-
-#     def fit(self, df):
-#         self.scaler_ = scalarize(df, self.power_transformer, self.robust_scalar)
-#         return self
-
-#     def transform(self, df):
-#         if self.scaler_ is None:
-#             raise ValueError("You must call fit() before transform()")
-#         return self.scaler_.transform(df)
-
-#     def fit_transform(self, df):
-#         return self.fit(df).transform(df)
+    def inference(self, df):
+        if self.transform:
+            return self.scaler_.transform(df)
+        raise ValueError("You must call fit() before inference()")
+    
