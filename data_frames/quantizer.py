5df2cc3661bf835c5e09a2ec4498bdeb2a684d7d
ziminqian <ziminq@stanford.edu>
2025-08-12 10:58:00 -0700

    quantize

--- data_frames/quantizer.py
+++ data_frames/quantizer.py
@@ -1,9 +1,15 @@
-from sklearn.preprocessing import KBinsDiscretizer
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import KBinsDiscretizer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import PowerTransformer, RobustScaler
 
 
-def quantize(df) :
+def bin(
+        df: pd.DataFrame, 
+        robust_scalar: bool = True, 
+        power_transform: bool = False
+        ) -> pd.DataFrame: 
     """
     Quantizes the numerical features of a DataFrame into discrete bins.
     
@@ -13,14 +19,44 @@ def quantize(df) :
     Returns:
     pd.DataFrame: DataFrame with quantized numerical features.
     """
+    """
+    Scales the numerical features of a DataFrame using RobustScaler or .
+    
+    Parameters:
+    df (pd.DataFrame): Input DataFrame with numerical features to scale.
+    
+    Returns:
+    pd.DataFrame: DataFrame with scaled numerical features.
+    """
+    #SCALING
     
     # Check if the DataFrame is empty
     if df.empty:
-        return df
+        raise ValueError("Input DataFrame is empty.")
+       
+    # Ensure only numeric columns
+    numeric_cols = df.select_dtypes(include="number").columns
+    if len(numeric_cols) == 0:  # Fixed the bug
+        raise ValueError("No numeric columns to transform.")
+    
+    # Impute missing values with median
+    imputer = SimpleImputer(strategy="median")
+    X = imputer.fit_transform(df[numeric_cols])
 
-    numeric_df = df.select_dtypes(include=['number'])
+    if power_transform:      
+        pt = PowerTransformer(method="yeo-johnson", standardize=True)
+        X = pt.fit_transform(X)
 
-    X = numeric_df.to_numpy()
+    if robust_scalar:
+        rs = RobustScaler()
+        X = rs.fit_transform(X)
+
+    df_scaled = pd.DataFrame(
+        X,
+        infex=df.index,
+        columns=numeric_cols
+    )
+    
     n_bins = 256
 
     # Choose 'uniform' or 'quantile' strategy
