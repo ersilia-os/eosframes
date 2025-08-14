e1cf17ef59015f0be97a052c6b967d5e2bf3e6be
ziminqian <ziminq@stanford.edu>
2025-08-14 16:23:19 -0700

    quantize test

--- test_quantize.py
+++ test_quantize.py
@@ -9,8 +9,6 @@ df_feature1 = df.iloc[:, [2]] #column 2 is the feature "abc"
 kbd = Quantize(robust_scaler=True, power_transform=False)
 
 fitted_df = kbd.fit(df_feature1) 
-fitted_df.to_csv('quantized_all.csv')
-
 # Save the model
 kbd.save('modred_dir')
 
@@ -18,12 +16,12 @@ kbd.save('modred_dir')
 # Later, load the model
 loaded_kbd = kbd.load('modred_dir')
 # Use the loaded model for inference
-df_inference = pd.read_csv("output_all.csv")
-df_inference1 = df_inference.iloc[:, [2]] #column 2 is the feature "abc"
+df_inference = pd.read_csv("ten_inf_numeric.csv")
+df_inference1 = df_inference.iloc[:, [3]] #column 2 is the feature "abc"
 
 
 result = loaded_kbd.inference(df_inference1)
-result.to_csv('inf_all.csv', index=False)
+result.to_csv('inf_five.csv', index=False)
 
 
 
