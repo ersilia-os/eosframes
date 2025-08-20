b4d01fc8d981b6cc3f2b7bc80cf7f68960c11996
patrickwalsh26 <walshp26@stanford.edu>
2025-08-20 11:17:58 -0500

    build_typed_transformer

---
+++ test_18.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from data_frames.transformers.scale import Scale
+from data_frames.transformers.build_typed_transformer import build_typed_transformer
+
+df_train = pd.read_csv("drugbank_output.csv")
+X = df_train.drop(columns=["key","input"], errors="ignore")
+preproc, groups = build_typed_transformer(X)
+preproc.fit(X)    # persist with joblib.dump(preproc, "eos78ao_typed_preproc.joblib")
+X_trans = preproc.transform(X)
+
