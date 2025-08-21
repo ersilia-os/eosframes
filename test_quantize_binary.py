e269d0a8596786613bad02d3d4b355f8234d7f72
ziminqian <ziminq@stanford.edu>
2025-08-20 22:25:33 -0700

    test binary quantize

--- test_quantize_binary.py
+++ test_quantize_binary.py
@@ -3,22 +3,18 @@ import pandas as pd
 import joblib
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("output_all.csv")
-df_feature1 = df.iloc[:, [2]] #column 2 is the feature "abc"
+df = pd.read_csv("test_binary_constant_fit.csv")
 
 kbd = Quantize(robust_scaler=True, power_transform=False)
 
-fitted_df = kbd.fit(df_feature1) 
+fitted_df = kbd.fit(df) 
 # Save the model
 kbd.save('modred_dir')
-
 
 # Later, load the model
 loaded_kbd = kbd.load('modred_dir')
 # Use the loaded model for inference
-df_inference = pd.read_csv("ten_inf_numeric.csv")
-df_inference1 = df_inference.iloc[:, [3]] #column 2 is the feature "abc"
+df_inference = pd.read_csv("test_binary_constant.csv")
 
-
-result = loaded_kbd.inference(df_inference1)
-result.to_csv('inf_five.csv', index=False)
+result = loaded_kbd.inference(df_inference)
+result.to_csv('binary-def-contin.csv', index=False)
